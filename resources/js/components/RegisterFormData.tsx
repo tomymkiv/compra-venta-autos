3319ff6c42ad214800b58104b1ad1cfc7a5a972e
Tomas <liberfran.04@gmail.com>
2026-05-25 12:48:06 -0300

    micro-correccion: habia un limite de caracteres en el input para el contacto

--- resources/js/components/RegisterFormData.tsx
+++ resources/js/components/RegisterFormData.tsx
@@ -14,7 +14,7 @@ export default function RegisterFormData({ errorMsg, type, name, setData, value 
         <div className="flex flex-col gap-2">
             <p className="text-red-600">{errorMsg}</p>
             <label htmlFor={name}>{name}</label>
-            <InputComp type={type} max={8} name={name} id={name} value={value} onChange={setData} placeholder={name} />
+            <InputComp type={type} name={name} id={name} value={value} onChange={setData} placeholder={name} />
         </div>
     )
 }
