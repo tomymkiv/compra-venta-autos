552ed699c8adde476f029b1283e8102ccaef70f6
Tomas <liberfran.04@gmail.com>
2026-07-28 17:25:54 -0300

    Pequeños ajustes a mobile

--- resources/js/components/DropdownButton.tsx
+++ resources/js/components/DropdownButton.tsx
@@ -9,7 +9,7 @@ export default function DropdownButton({ title, onclick, onmouseenter, onmousele
     return <li
         onClick={onclick}
         className={`text-[#ccc] pl-5`}>
-        <button className={`cursor-pointer text-xl flex items-center lg:w-[85%] gap-2 hover:bg-gray-500/20 p-3 lg:transition-background lg:duration-300 rounded-md hover:scale-105`}
+        <button className={`cursor-pointer text-xl flex items-center w-fit lg:w-[85%] gap-2 lg:hover:bg-gray-500/20 p-3 lg:transition-background lg:duration-300 rounded-md lg:hover:scale-105`}
             onMouseEnter={onmouseenter}
             onMouseLeave={onmouseleave}>{title}</button>
     </li>;
--- resources/js/components/PageLinks.tsx
+++ resources/js/components/PageLinks.tsx
@@ -9,7 +9,7 @@ interface PageLinksProps {
 
 export default function PageLinks({ title, link, onclick, metodo }: PageLinksProps) {
     return <li className={`text-[#ccc] pl-5`}>
-        <Link method={metodo ?? 'get'} href={link} className="text-xl flex items-center lg:w-[85%] gap-2 hover:bg-gray-500/20 p-3 lg:transition-background lg:duration-300 rounded-md hover:scale-105 cursor-pointer" onClick={onclick}>
+        <Link method={metodo ?? 'get'} href={link} className="text-xl flex items-center w-fit lg:w-[85%] gap-2 lg:hover:bg-gray-500/20 p-3 lg:transition-background lg:duration-300 rounded-md lg:hover:scale-105 cursor-pointer" onClick={onclick}>
             {title}
         </Link>
     </li>;
